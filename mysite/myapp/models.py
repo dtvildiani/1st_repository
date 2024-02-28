14d75a83a53b391c97a3c8c418607e37bd1f3baa
Davit Tvildiani <davit.tvildiani@gmail.com>
2024-02-29 00:32:36 +0400

    added images in models

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=20)
     price = models.DecimalField(decimal_places=2, max_digits=10)
     description = models.TextField()
+    image = models.ImageField(blank=True, null=True, upload_to='images')
 
     def __str__(self):
         return self.name
