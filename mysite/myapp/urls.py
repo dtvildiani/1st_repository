32c340e9f4c8b540f8e73380c79e16136bd2f982
Davit Tvildiani <davit.tvildiani@gmail.com>
2024-03-04 23:24:16 +0400

    Additem.html with save function

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import indexItem
 from .views import index
+from .views import add_item
 app_name = 'myapp'
 
 urlpatterns = [
     path('', index),
     path('<int:my_id>/', indexItem, name='detail'),
+    path('additem/', add_item, name='add_item'),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -18,3 +18,13 @@ def indexItem(request, my_id):
         'item': item
     }
     return render(request, 'detail.html', context=context)
+
+def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        image = request.FILES['upload']
+        item = Product(name=name, price=price, description=description, image=image)
+        item.save()
+    return render(request, 'additem.html')
